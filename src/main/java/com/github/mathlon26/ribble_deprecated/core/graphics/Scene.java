f7410ce7a1db6248a3b929d88e17b3288cfe2bcb
Mathijs Follon <mathijs.follon@student.uhasselt.be>
2025-09-18 18:48:53 +0200

    Remove deprecated

--- src/main/java/com/github/mathlon26/ribble_deprecated/core/graphics/Scene.java
+++
@@ -1,9 +0,0 @@
-package com.github.mathlon26.ribble_deprecated.core.graphics;
-
-public abstract class Scene {
-
-    public Scene() {}
-
-    public abstract void update(double dt);
-    public abstract void render();
-}
